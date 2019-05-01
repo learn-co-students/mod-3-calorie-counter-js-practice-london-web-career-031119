ac1c9eb61a7e2554762c506893acd57c98348a81
github username <github email address>
2019-05-01 17:24:10 +0100

    Done.

---
+++ food-data.js
@@ -0,0 +1,8 @@
+FOODS = [
+  {calorie: 580, note: "Poke Bar - Medium Poke Bowl", id: 1},
+  {calorie: 208, note: "Taiyaki - Ice-cream cone", id: 2},
+  {calorie: 800, note: "Taco Truck - Chicken Burrito", id: 3},
+  {calorie: 266, note: "Vegetable Kofta", id: 4},
+  {calorie: 304, note: "Kung Fu Tea - Passion fruit green tea with boba", id: 5},
+  {calorie: 918, note: "Bodega down the block - Cheese burger with all the topings...", id: 6}
+]
